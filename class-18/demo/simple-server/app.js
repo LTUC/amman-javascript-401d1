8610b14f95e4f9b9584344b853282f4418a28b8f
Ahmad Alawad <ahmad.alawad.sf@gmail.com>
2020-02-11 14:37:30 +0200

    modified the naming of demo apps

--- class-18/demo/simple-server/app.js
+++
@@ -1,7 +0,0 @@
-const io = require('socket.io-client');
-const socket = io.connect('http://localhost:3000');
-const faker = require('faker');
-
-setInterval( () => {
-  socket.emit('speak', faker.hacker.phrase());
-}, 750);
